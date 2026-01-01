76e4a9bb462f4b03de94ab34ab00eaa84b904ce1
Thomas <thomashung076@gmail.com>
2026-01-01 18:42:53 +0000

    nothing

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  experimental: {
+    cacheComponents: true,
+  },
   images: {
     remotePatterns: [
       {
